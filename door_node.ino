377fc1a88c0f468f1e13b2ef0666b27d87dc882e
PattexGamer <Tiesje1998@hotmail.com>
2017-10-06 21:03:50 +0200

    Update door_node.ino

--- door_node.ino
+++ door_node.ino
@@ -1,53 +1,44 @@
 #include <DallasTemperature.h>
 #include <OneWire.h>
 
-
-// Enable debug prints to serial monitor
 #define MY_DEBUG
 
 #define MY_RADIO_NRF24
-#define MY_NODE_ID 50
+#define MY_NODE_ID 49 // DEMO Node
 
-
-#define CHILD_ID_TEMP_MIDDLE 1
-
+#define CHILD_ID_DOOR_MAIN_OPEN 5
+#define CHILD_ID_DOOR_MAIN_TEMP 6
 
 #include <MySensors.h>
 
-#define SKETCH_NAME "Binary Sensor"
+#define SKETCH_NAME "DEMO-ROOM Door node"
 #define SKETCH_MAJOR_VER "1"
 #define SKETCH_MINOR_VER "0"
-
-#define PRIMARY_CHILD_ID 3
 
 static const uint8_t FORCE_UPDATE_N_READS = 5;
 
 #define PRIMARY_BUTTON_PIN 2   // Arduino Digital I/O pin for button/reed switch
-
-// Temperature sensor
 #define ONE_WIRE_BUS 4 // Pin where dallase sensor is connected 
+
 OneWire oneWire(ONE_WIRE_BUS); 
 DallasTemperature sensors(&oneWire); 
 
-float lastTemperature;
 uint8_t nNoUpdatesTemp;
-MyMessage tempMsg(CHILD_ID_TEMP_MIDDLE,V_TEMP);
+unsigned long sleepTime = 30000;
 
-
-// Change to V_LIGHT if you use S_LIGHT in presentation below
-MyMessage msg(PRIMARY_CHILD_ID, V_TRIPPED);
+MyMessage tempMsg(CHILD_ID_DOOR_MAIN_TEMP, V_TEMP);
+MyMessage msg(CHILD_ID_DOOR_MAIN_OPEN, V_TRIPPED);
 
 void before()
 {
     sensors.begin();
-
 }
 
 void setup()
 {
   // Setup the buttons
   pinMode(PRIMARY_BUTTON_PIN, INPUT);
-
+  
   // Activate internal pull-ups
   digitalWrite(PRIMARY_BUTTON_PIN, HIGH);
 }
@@ -58,9 +49,8 @@ void presentation()
   sendSketchInfo(SKETCH_NAME, SKETCH_MAJOR_VER "." SKETCH_MINOR_VER);
 
   // Register binary input sensor to sensor_node (they will be created as child devices)
-  present(PRIMARY_CHILD_ID, S_DOOR);
-    present(CHILD_ID_TEMP_MIDDLE,S_TEMP);
-
+  present(CHILD_ID_DOOR_MAIN_OPEN, S_DOOR);
+  present(CHILD_ID_DOOR_MAIN_TEMP, S_TEMP);
 }
 
 // Loop will iterate on changes on the BUTTON_PINs
@@ -81,19 +71,16 @@ void loop()
   }
 
   processTemperature();
-
   // Sleep until something happens with the sensor
-  sleep(PRIMARY_BUTTON_PIN-2, CHANGE, 10000);
+  sleep(PRIMARY_BUTTON_PIN-2, CHANGE, sleepTime);
 }
-
 
 void processTemperature()
 {
   sensors.requestTemperatures();
   float temperature =  sensors.getTempCByIndex(0); 
 
-  if ((lastTemperature != temperature && temperature != -127.00 && temperature != 85.00) || nNoUpdatesTemp == FORCE_UPDATE_N_READS) {
-    lastTemperature=temperature;  
+  if ((temperature != -127.00 && temperature != 85.00) || nNoUpdatesTemp == FORCE_UPDATE_N_READS) {
     nNoUpdatesTemp = 0;
     send(tempMsg.set(temperature,1));
   } else {
